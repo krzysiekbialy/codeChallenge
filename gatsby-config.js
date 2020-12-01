7aeb618a7e323659e4f2159eb5aa7d17ebd4656f
Krzysiek <krzysiek@aexol.com>
2020-12-01 15:16:30 +0100

    fixed gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,12 @@
 const path = require("path"); // eslint-disable-line
 
 module.exports = {
-  pathPrefix: "/codeChallenge.com",
+  pathPrefix: "krzysiekbialy.github.io/codeChallenge",
   siteMetadata: {
     title: "Aexol - Innovative Software Development studio",
     description:
       "We are the team responsible for GraphQL Editor Stucco and Foodeli platforms. We can work as your software consultants.",
     siteUrl: "https://codeChallenge.com", // full path to blog - no ending slash
-  },
-  mapping: {
-    "MarkdownRemark.frontmatter.author": "AuthorYaml",
   },
   plugins: [
     "gatsby-plugin-sitemap",
