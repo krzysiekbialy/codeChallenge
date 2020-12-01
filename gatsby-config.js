ec89793cd0dc812c71be2a7d6ff4d393d0f96463
Krzysiek <krzysiek@aexol.com>
2020-12-01 15:36:21 +0100

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,6 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-yaml",
-    "gatsby-plugin-feed",
     {
       resolve: "gatsby-plugin-postcss",
       options: {
